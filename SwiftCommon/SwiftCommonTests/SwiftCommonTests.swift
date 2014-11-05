33630e438b5a16c0a7e673e2220d225b768a3cf7
Sgt. Fancy Pants <grant.hughes@emerson.com>
2014-11-05 14:43:20 -0500

    Initial commit of the project

---
+++ SwiftCommon/SwiftCommonTests/SwiftCommonTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftCommonTests.swift
+//  SwiftCommonTests
+//
+//  Created by Grant Hughes on 11/5/14.
+//  Copyright (c) 2014 RIDGID. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SwiftCommonTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
